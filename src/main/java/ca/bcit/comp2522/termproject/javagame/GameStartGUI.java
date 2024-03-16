9affe4cdd0939d24181d3c4e913ed8288e9d60d2
Caroline Su <caroline77899@gmail.com>
2024-03-15 23:43:52 -0700

    Add slime icon

--- src/main/java/ca/bcit/comp2522/termproject/javagame/GameStartGUI.java
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/GameStartGUI.java
@@ -2,8 +2,13 @@ package ca.bcit.comp2522.termproject.javagame;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameStartGUI extends Application {
@@ -16,11 +21,12 @@ public class GameStartGUI extends Application {
         startGameButton.setShape(circle);
         startGameButton.setMinSize(100, 100);
         startGameButton.setMaxSize(100, 100);
+        startGameButton.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
 
-        // 为按钮设置点击事件处理器
+        // add event listener to the button
         startGameButton.setOnAction(event -> {
             // Caroline: WRITE GAME START AND INITIALIZE CODE HERE!!!!!!
-            System.out.println("游戏开始！");
+            System.out.println("Game Start!");
         });
 
         //创建一个StackPane，把Button添加到布局中
@@ -28,10 +34,15 @@ public class GameStartGUI extends Application {
         root.getChildren().add(startGameButton);
 
         // 创建scene，设置scene大小，把StackPane设置到场景中
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 500, 500);
 
-        // 设置Stage的标题和scene，然后show
-        primaryStage.setTitle("Game start GUI");
+        Image icon = new Image("yellowSlime.png");
+        primaryStage.getIcons().add(icon);
+
+        // set primaryStage title and not able to be resized
+        primaryStage.setTitle("Slime Container");
+        primaryStage.setResizable(false);
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
