0f68ba4265203f691870b6afaf7e144747a43895
Caroline Su <caroline77899@gmail.com>
2024-03-15 23:44:09 -0700

    Add getters and setters

--- src/main/java/ca/bcit/comp2522/termproject/javagame/Slime.java
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/Slime.java
@@ -1,10 +1,69 @@
 package ca.bcit.comp2522.termproject.javagame;
 
 public abstract class Slime {
-    protected int size;
+    private int size;
+    private double xCoordinator;
+    private double yCoordinator;
+    private double xVelocity;
+    private double yVelocity;
+    private int price;
+    private boolean isAlive;
+    public int getSize() {
+        return this.size;
+    }
+
+    public double getXCoordinator() {
+        return this.xCoordinator;
+    }
+
+    public void setXCoordinator(double newXCoordinator) {
+        this.xCoordinator = newXCoordinator;
+    }
+
+    public double getYCoordinator() {
+        return this.yCoordinator;
+    }
+
+    public void setYCoordinator(double newYCoordinator) {
+        this.yCoordinator = newYCoordinator;
+    }
+
+    public double getXVelocity() {
+        return this.xVelocity;
+    }
+
+    public void setXVelocity(double newXVelocity) {
+        this.xVelocity = newXVelocity;
+    }
+
+    public double getYVelocity() {
+        return this.yVelocity;
+    }
+
+    public void setYVelocity(double newYVelocity) {
+        this.yVelocity = newYVelocity;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int newPrice) {
+        this.price = newPrice;
+    }
+
+    public boolean getIsAlive() {
+        return this.isAlive;
+    }
+
+    public void setAlive(boolean newIsAlive) {
+        if (this.isAlive) {
+            this.isAlive = newIsAlive;
+        }
+    }
+
+
 
     protected abstract SlimeType setConstantSlimeType(SlimeType slimeType);
-    protected abstract int setConstantSize(int size);
-    protected abstract int setConstantPrice(int size);
-    protected abstract void setSize(int newSize);
+    protected abstract String setConstantSlimeImage(String imgName);
 }
