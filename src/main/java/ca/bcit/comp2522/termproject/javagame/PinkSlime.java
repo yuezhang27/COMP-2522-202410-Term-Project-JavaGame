650f48f412cefdb190a8caf710054ed05da2b666
Caroline Su <caroline77899@gmail.com>
2024-03-16 21:02:20 -0700

    add PinkSlime class

---
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/PinkSlime.java
@@ -0,0 +1,39 @@
+package ca.bcit.comp2522.termproject.javagame;
+
+import java.util.ArrayList;
+
+public class PinkSlime extends Slime{
+    public PinkSlime(double xPosition, double yPosition) {
+        super(xPosition, yPosition);
+    }
+
+    @Override
+    protected SlimeType setConstantSlimeType(SlimeType slimeType) {
+        return null;
+    }
+
+    @Override
+    protected String setConstantSlimeImage(String imgName) {
+        return null;
+    }
+
+    @Override
+    protected void moveSlime(Slime slime) {
+
+    }
+
+    @Override
+    protected ArrayList<Slime> splitSlime(Slime slime) {
+        return null;
+    }
+
+    @Override
+    protected boolean checkIsCollide() {
+        return false;
+    }
+
+    @Override
+    protected Slime slimeMutation() {
+        return null;
+    }
+}
