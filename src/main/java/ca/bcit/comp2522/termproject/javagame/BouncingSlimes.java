4dee9724452e6ebb6124b0039e715460df65e479
Caroline Su <caroline77899@gmail.com>
2024-04-02 19:07:28 -0700

    refactor and tidy up code

--- src/main/java/ca/bcit/comp2522/termproject/javagame/BouncingSlimes.java
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/BouncingSlimes.java
@@ -9,11 +9,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.ListCell;
-
-
 
 
 /**
@@ -26,35 +26,64 @@ import javafx.scene.control.ListCell;
 public class BouncingSlimes extends Application {
 
 
-    private static double balance;
-    public static double getBalance() {
-        return balance;
-    }
-
-//    public static void setBalance(double balance) {
-//        BouncingSlimes.balance = balance;
-//    }
-
-
-
     /**
      * Demonstrates threading in JavaFX.
+     *
      * @param primaryStage contains the Scene
      */
     public void start(final Stage primaryStage) {
-        Image backgroundImg = new Image("regularPetriDish.png");
-        ImageView imageView = new ImageView(backgroundImg);
-
+//        set up canvas
         PetriDish petriDish = new PetriDish();
-        Pane canvas = petriDish.getCanvas();
-        canvas.getChildren().add(imageView);
+        Pane canvas = setupCanvas(petriDish);
+
         Scene scene = new Scene(canvas, 500, 500);
         scene.getStylesheets().add("style.css");
+
+        Button sellButton = createSellBtn();
+
+        AnchorPane slimeListCanvas = new AnchorPane();
+
+        //Sell button onclick event handler
+        sellButton.setOnAction(event -> {
+            System.out.println("Sell button clicked");
+            ListView<Slime> slimeListView = createSlimeListView(petriDish);
+            slimeListCanvas.getChildren().addFirst(slimeListView);
+            slimeListCanvas.toFront();
+            // AVOID REPETITION
+            canvas.getChildren().removeIf(node -> node instanceof ListView);
+            canvas.getChildren().add(slimeListCanvas);
+        });
+
+        canvas.getChildren().add(sellButton);
+
+        //HARD CODE BALANCE FOR 500 FOR NOW!!
+        Player player = new Player();
+        Label balanceLabel = createBalanceLabel(player);
+        canvas.getChildren().add(balanceLabel);
+        Image icon = new Image("pinkSlime.png");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("Threads and Balls");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+//        add a default slime to start the game
+        addDefaultSlime(canvas, petriDish);
+
+    }
+
+    private Pane setupCanvas(PetriDish petriDish) {
+        Image backgroundImg = new Image("regularPetriDish.png");
+        ImageView imageView = new ImageView(backgroundImg);
+        Pane canvas = petriDish.getCanvas();
+        canvas.getChildren().add(imageView);
+        return canvas;
+    }
+
+    private Button createSellBtn() {
         Button sellButton = new Button("Sell Slimes");
         sellButton.getStyleClass().add("button-sell");
         //Logo of Sell button
         Image sellButtonImage = new Image("SellButton.png");
-//        Image sellButtonImage = new Image("purpleSlime.png");
         ImageView sellImageView = new ImageView(sellButtonImage);
 
         sellImageView.setFitHeight(25);  //Button height
@@ -65,47 +94,43 @@ public class BouncingSlimes extends Application {
         //Coordination of sell button
         sellButton.setLayoutX(25);
         sellButton.setLayoutY(10);
-        //Sell button onclick event handler
-        sellButton.setOnAction(event -> {
-            System.out.println("Sell button clicked");
-            ListView<Slime> slimeListView = new ListView<>(FXCollections.observableArrayList(petriDish.getSlimesList()));
-            slimeListView.setCellFactory(lv -> new ListCell<Slime>() {
-                @Override
-                protected void updateItem(Slime slime, boolean empty) {
-                    super.updateItem(slime, empty);
-                    if (empty || slime == null) {
-                        setText(null);
-                    } else {
-                        setText("Slime " + slime.getSlimeId() + " - $" + slime.getPrice());
-                    }
-                }
-            });
-            // Coordination X, Y of the Listview (Slime list)
-            slimeListView.setLayoutX(50);
-            slimeListView.setLayoutY(100);
-            slimeListView.setPrefSize(200, 300);
+        return sellButton;
+    }
 
-            // AVOID REPETITION
-            canvas.getChildren().removeIf(node -> node instanceof ListView);
-            canvas.getChildren().add(slimeListView);
-        });
-
-        canvas.getChildren().add(sellButton);
-        //HARD CODE BALANCE FOR 500 FOR NOW!!
-        Label balanceLabel = new Label("Balance $: "+getBalance());
+    private Label createBalanceLabel(Player player) {
+        Label balanceLabel = new Label("\uD83D\uDCB0Balance $: " + player.getBalance());
         balanceLabel.setLayoutX(300);
         balanceLabel.setLayoutY(10);
         balanceLabel.getStyleClass().add("balance-label");
-        canvas.getChildren().add(balanceLabel);
+        balanceLabel.setFont(Font.font("Segoe UI Emoji"));
+        return balanceLabel;
+    }
+
+    private ListView<Slime> createSlimeListView(PetriDish petriDish) {
+        ListView<Slime> slimeListView = new ListView<>(FXCollections.observableArrayList(petriDish.getSlimesList()));
+        slimeListView.setCellFactory(lv -> new ListCell<Slime>() {
+            @Override
+            protected void updateItem(Slime slime, boolean empty) {
+                super.updateItem(slime, empty);
+                if (empty || slime == null) {
+                    setText(null);
+                } else {
+                    setText(slime.getName() + slime.getSlimeId() + " - $" + slime.getPrice());
+                }
+            }
+        });
+        // Coordination X, Y of the Listview (Slime list)
+        slimeListView.setLayoutX(50);
+        slimeListView.setLayoutY(100);
+        slimeListView.setPrefSize(200, 300);
+        return slimeListView;
+    }
+
+
+    private void addDefaultSlime(Pane canvas, PetriDish petriDish) {
         Slime defaultSlime = new YellowSlime(250, 250, petriDish);
         defaultSlime.addToPane(canvas);
         defaultSlime.startThread();
-        Image icon = new Image("pinkSlime.png");
-        primaryStage.getIcons().add(icon);
-        primaryStage.setTitle("Threads and Balls");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
     }
 
     /**
