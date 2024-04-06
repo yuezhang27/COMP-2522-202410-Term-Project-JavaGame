8065b0217d99ca5dcfefd6f0401d3f1511ac1f6c
Caroline Su <caroline77899@gmail.com>
2024-04-05 23:00:47 -0700

    add javadoc in Player

--- src/main/java/ca/bcit/comp2522/termproject/javagame/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/Player.java
@@ -1,27 +1,51 @@
 package ca.bcit.comp2522.termproject.javagame;
 
+/**
+ * Represents a player with a balance.
+ *
+ * @author Caroline Su GitHub:Juntingg
+ * @author Kim Zhang GitHub:yuezhang27
+ * @version 2024
+ */
 public class Player {
+    private static final int INITIAL_BALANCE = 10;
     private int balance;
 
+    /**
+     * Constructs a new player with an initial balance of 10.
+     */
     public Player() {
-        this.balance = 10;
-
+        this.balance = INITIAL_BALANCE;
     }
-    public void increaseBalance(int income) {
+
+    /**
+     * Increases the player's balance by the specified income.
+     *
+     * @param income the amount to increase the balance by
+     */
+    public void increaseBalance(final int income) {
         this.balance += income;
     }
 
-    public boolean reduceBalance(int outcome) {
+    /**
+     * Reduces the player's balance by the specified outcome.
+     *
+     * @param outcome the amount to reduce the balance by
+     * @return true if the balance was successfully reduced, false if the outcome exceeds the current balance
+     */
+    public boolean reduceBalance(final int outcome) {
         if (outcome > this.balance) {
             return false;
         }
         this.balance -= outcome;
         return true;
     }
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
 
+    /**
+     * Gets the current balance of the player.
+     *
+     * @return the current balance
+     */
     public int getBalance() {
         return this.balance;
     }
