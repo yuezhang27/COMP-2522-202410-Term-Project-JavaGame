c05b1e44d0b5569a48ef2817d19de6c7fa8c98ce
Caroline Su <caroline77899@gmail.com>
2024-04-05 23:01:21 -0700

    add javadoc to GameStarGUI

--- src/main/java/ca/bcit/comp2522/termproject/javagame/GameStartGUI.java
+++ src/main/java/ca/bcit/comp2522/termproject/javagame/GameStartGUI.java
@@ -1,4 +1,5 @@
 package ca.bcit.comp2522.termproject.javagame;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,56 +12,74 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.application.Platform;
 
+/**
+ * GameStartGUI class represents the GUI for starting the game.
+ *
+ * @author Caroline Su GitHub:Juntingg
+ * @author Kim Zhang GitHub:yuezhang27
+ * @version 2024
+ */
 public class GameStartGUI extends Application {
+    private static final int BUTTON_SIZE = 100;
+    private static final int BUTTON_TRANSLATE_Y = 100;
+    private static final int CIRCLE_RADIUS = 50;
+    private static final int FONT_SIZE = 20;
 
+    /**
+     * Starts the application by creating the primary stage with a background image and a start game button.
+     *
+     * @param primaryStage The primary stage of the JavaFX application.
+     */
     @Override
-    public void start(Stage primaryStage) {
-        Image backgroundImg = new Image("slimeBackground.png");
-        ImageView imageView = new ImageView(backgroundImg);
+    public void start(final Stage primaryStage) {
 
-        // 创建一个Button
-        Button startGameButton = new Button("START GAME!");
-        Circle circle = new Circle(50);
-        startGameButton.setShape(circle);
-        startGameButton.setMinSize(100, 100);
-        startGameButton.setMaxSize(100, 100);
-        startGameButton.setTranslateY(100);
-        startGameButton.setFont(Font.font("Verdana", FontWeight.BOLD, 10));
-
-        //connect the 2 stage
-        startGameButton.setOnAction(event -> {
-            System.out.println("Game Start!");
-            primaryStage.close();
-            Platform.runLater(() -> {
-                try {
-                    new BouncingSlimes().start(new Stage());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        });
-
-
-        //创建一个StackPane，把Button添加到布局中
+        ImageView background = createBackground();
+        Button startGameButton = createStartGameButton(primaryStage);
         StackPane root = new StackPane();
-        root.getChildren().add(imageView);
-        root.getChildren().add(startGameButton);
+        root.getChildren().addAll(background, startGameButton);
 
-        // 创建scene，设置scene大小，把StackPane设置到场景中
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(root, BouncingSlimes.WINDOW_SIZE_X, BouncingSlimes.WINDOW_SIZE_Y);
+        setupPrimaryStage(primaryStage, scene);
+        primaryStage.show();
+    }
 
+    private ImageView createBackground() {
+        Image backgroundImg = new Image("slimeBackground.png");
+        return new ImageView(backgroundImg);
+    }
+
+    private Button createStartGameButton(final Stage primaryStage) {
+        Button startGameButton = new Button("START\nGAME!");
+        Circle circle = new Circle(CIRCLE_RADIUS);
+        startGameButton.setShape(circle);
+        startGameButton.setPrefSize(BUTTON_SIZE, BUTTON_SIZE);
+        startGameButton.setTranslateY(BUTTON_TRANSLATE_Y);
+        startGameButton.setFont(Font.font("Verdana", FontWeight.BOLD, FONT_SIZE));
+        startGameButton.setOnAction(actionEvent -> startGame(primaryStage));
+        return startGameButton;
+    }
+
+    private void setupPrimaryStage(final Stage primaryStage, final Scene scene) {
         Image icon = new Image("yellowSlime.png");
         primaryStage.getIcons().add(icon);
 
-        // set primaryStage title and not able to be resized
         primaryStage.setTitle("Slime Container");
         primaryStage.setResizable(false);
 
         primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    private void startGame(final Stage primaryStage) {
+        primaryStage.close();
+        Platform.runLater(() -> new BouncingSlimes().start(new Stage()));
+    }
+
+    /**
+     * The entry point for launching the application.
+     *
+     * @param args The command-line arguments.
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
 }
